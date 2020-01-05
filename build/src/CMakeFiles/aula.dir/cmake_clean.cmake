c3fbeb1fa0783a1fad112eeb39b9e64bb2033e1f
Marcos <marcosdossantos43@hotmail.com>
2020-01-05 18:35:57 -0300

    first cmake include qt5

---
+++ build/src/CMakeFiles/aula.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "qrc_resources.cpp"
+  "CMakeFiles/aula.dir/main.cpp.o"
+  "CMakeFiles/aula.dir/qrc_resources.cpp.o"
+  "aula.pdb"
+  "aula"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aula.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ qml/main.qml
@@ -0,0 +1,15 @@
+import QtQuick 2.7
+import QtQuick.Controls 1.0
+
+ApplicationWindow {
+    id: window
+    title: "Video-Player"
+    visible: true
+    width: 600
+    height: 400
+
+    Rectangle {
+        anchors.fill: parent
+        color: "blue"
+    }
+}
